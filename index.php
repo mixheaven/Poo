125f8158a531bd57d9e58a7cd72da682a6eeac1f
mixheaven <53529921+mixheaven@users.noreply.github.com>
2021-05-28 09:56:00 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,51 +1,18 @@
 <?php
 
 
-require_once 'Bicycle.php';
-$bike = new Bicycle();
-$bike->setColor('blue');
-$bike->setCurrentSpeed(0);
-
-// Instanciation d'un nouvel objet $rockrider
-$rockrider = new Bicycle();
-$rockrider->setColor('yelow');
-
-// Instanciation d'un nouvel objet $tornado
-$tornado = new Bicycle();
-$tornado->setColor('black');
-$tornado->setCurrentSpeed(0);
-
-// Instanciation d'une voiture
 require_once 'Car.php';
-$mustang = new Car('red', 4,'nitrogène' );
-echo $mustang->start();
-echo '<br> Vitesse de la voiture ; ' .$mustang->getCurrentSpeed() . 'km/h' . '<br>';
-echo $mustang->forward();
-echo '<br> Vitesse de la voiture ; ' .$mustang->getCurrentSpeed() . 'km/h' . '<br>';
-echo $mustang->brake();
-echo '<br> Vitesse de la voiture ; ' .$mustang->getCurrentSpeed() . 'km/h' . '<br>';
+require_once 'Bicycle.php';
+require_once 'Truck.php';
 
+$truck = new Truck('green', 3 , 50);
+$truck->setCurrentSpeed(40);
+$truck->SetStorage(50);
 
-
-
-var_dump($bike);
-$bike->dump();
-
-
-// Moving bike
-echo $bike->forward();
-echo '<br> Vitesse du vélo : ' . $bike->getCurrentSpeed() . ' km/h' . '<br>';
-echo $bike->brake();
-echo '<br> Vitesse du vélo : ' . $bike->getCurrentSpeed() . ' km/h' . '<br>';
-echo $bike->brake();
-// Moving bike
-echo $tornado->forward();
-echo '<br> Vitesse de tornado : ' . $tornado->getCurrentSpeed() . ' km/h' . '<br>';
-echo $tornado->brake();
-echo '<br> Vitesse de tornado: ' . $tornado->getCurrentSpeed() . ' km/h' . '<br>';
-echo $tornado->brake();
-
-
-
+echo $truck->forward() . '<br>';
+echo 'Vitesse du camion ' . $truck->getCurrentSpeed() . 'km/h' . '<br>';
+echo $truck->brake() . '<br>';
+echo 'Le camion posséde ' . $truck->getStorageLevel() . ' tone ';
+echo $truck->isFull() . ' le camion est chagré à ' . $truck->getStorageLevel() . 
 
 ?>
