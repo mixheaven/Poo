9b081e17ccbf97e67e321bf4c931aa8524b5c3e2
mixheaven <53529921+mixheaven@users.noreply.github.com>
2021-06-02 15:58:02 +0200

    Add files via upload

---
+++ Bicycle.php
@@ -0,0 +1,12 @@
+<?php
+
+
+class Bicycle extends Vehicle
+{
+   
+
+
+}
+
+
+?>
---
+++ index.php
@@ -0,0 +1,43 @@
+<?php
+
+require_once 'Vehicle.php';
+require_once 'Bicycle.php';
+require_once 'Car.php';
+require_once 'Truck.php';
+require_once 'MotorWay.php';
+require_once 'ResidentialWay.php';
+require_once 'PedestrianWay.php';
+
+var_dump(Car::ALLOWED_ENERGIES);
+
+$bicycle = new Bicycle("blue", 1);
+echo $bicycle->forward();
+var_dump($bicycle);
+
+$car = new Car('green', 4, 'electric');
+echo $car->forward();
+var_dump($car);
+
+$truck = new Truck('reed', 3, 'fuel', 100);
+$truck->SetStorage(50);
+echo $truck->forward();
+var_dump($truck);
+
+echo '<br> Vitesse du camion : ' . $truck->getCurrentSpeed() . ' km/h' . '<br>';
+echo $truck->brake();
+echo '<br> Vitesse du camion : ' . $truck->getCurrentSpeed() . ' km/h' . '<br>';
+echo $truck->brake() . '<br>';
+echo 'Le camion est: ' . $truck->getStorageLevel();
+
+$motorWay = new MotorWay(4, 130);
+$motorWay->addVehicle(new Car('green', 5, 'electric'));
+var_dump($motorWay);
+
+$residentialWay = new ResidentialWay(1, 10);
+$residentialWay->addVehicle(new Truck('white', 3, 'fuel', 200));
+var_dump($residentialWay);
+
+$pedestrianWay = new PedestrianWay(2, 50);
+$pedestrianWay->addVehicle(new Bicycle('orange', 1));
+var_dump($pedestrianWay);
+?> 
